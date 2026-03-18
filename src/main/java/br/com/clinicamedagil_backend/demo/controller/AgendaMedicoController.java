5d984f97cc7f4502aab766fad697a5662b8b8951
ChristianF22 <christianfonseca991@gmail.com>
2026-03-17 22:47:10 -0300

    feature/spring_security_jwt

--- src/main/java/br/com/clinicamedagil_backend/demo/controller/AgendaMedicoController.java
+++ src/main/java/br/com/clinicamedagil_backend/demo/controller/AgendaMedicoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +46,7 @@ public class AgendaMedicoController {
 
     @GetMapping
     @Operation(summary="Relação de AgendaMedico", description="Listar Todos Agendamentos Médicos")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MEDICO')")
     public ResponseEntity<List<AgendaMedicoDTO>> listarTodosAgendaMedicos() {
         List<AgendaMedicoDTO> lista = service.listarTodos()
                 .stream()
--- src/main/java/br/com/clinicamedagil_backend/demo/controller/HorarioAgendaController.java
+++ src/main/java/br/com/clinicamedagil_backend/demo/controller/HorarioAgendaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +46,7 @@ public class HorarioAgendaController {
 
     @GetMapping
     @Operation(summary="Relação de HorarioAgenda", description="Lista Todos Horários Agendados")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MEDICO')")
     public ResponseEntity<List<HorarioAgendaDTO>> listarTodosHorariosAgendas() {
         List<HorarioAgendaDTO> lista = service.listarTodos()
                 .stream()
